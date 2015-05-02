3a38427ebf7642ea4d30f7fda94b04a29d9f59f5
FreteNinja <freteninja@gmail.com>
2015-05-01 19:57:19 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Freteninja::Application.config.secret_key_base = '317e611ade817f05729e1b9707f629ed950806f4b64470545233f78fc3d94e52660a3ddbe0f4a1d742377601abeba07d52f212f206d2382eda44b2da2fc2e636'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Freteninja::Application.config.session_store :cookie_store, key: '_freteninja_session'
